a70a6a2fe921c89ca9ac1c78e6a2b2f525d4ec7d
Monspieeet <96837390+Monspieeet@users.noreply.github.com>
2023-06-26 16:17:12 +0300

    Create lab3.1.rb

---
+++ task3/lab3.1.rb
@@ -0,0 +1,26 @@
+def min(arr)
+    min = arr[0]
+    for i in arr
+        if i < min then
+            min = i
+        end
+    end
+    return min
+end    
+
+def first_pos(arr)
+    i = 0
+    while arr[i] <= 0 do
+        i += 1
+    end
+    return i
+end
+
+n = STDIN.gets.to_i
+arr = []
+0.upto(n-1) do |i|
+    arr[i] = STDIN.gets.to_i
+end
+
+puts min(arr)
+puts first_pos(arr)
