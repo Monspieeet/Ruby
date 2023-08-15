d9cb6d75f59c0c948da5016d7bc5c7eedfbbae29
Monspieeet <96837390+Monspieeet@users.noreply.github.com>
2023-08-15 20:52:13 +0300

    добавлены методы позволяющие получить элемент по номеру строки и столбца + узнать кол-во столбцов

--- Core classes and models/data_table.rb
+++ Core classes and models/data_table.rb
@@ -3,4 +3,16 @@ class Data_table
         @data = data
     end    
 
+    def get_element(row, col)
+        @data[row][col]
+    end
+
+    def get_rows_count
+        @data.size
+    end    
+
+    def get_colums_count
+        @data[0].size
+    end    
+    
 end
