c879a39a757ce1402895961861334f277c4ba8fc
Monspieeet <96837390+Monspieeet@users.noreply.github.com>
2023-08-18 00:40:15 +0300

    сделал переиндексацию объектов

--- Core classes and models/data_list.rb
+++ Core classes and models/data_list.rb
@@ -1,15 +1,19 @@
-
 class Data_list
   attr_reader :data, :selected
 
 
   def initialize(data)
-    @data = data
+    self.data = data
     @selected = []
   end
 
+  def data= (data)
+    index = 0
+    @data = data[:data].each { |student| student[:id] = index += 1 }
+  end
+  
   def select(number)
-    @selected << data[:data][number]
+    @selected << data[number]
   end
 
   def get_selected
