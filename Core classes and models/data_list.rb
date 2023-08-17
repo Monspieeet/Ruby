879272a6477d23be404efb860c040cdbd80e4d14
Monspieeet <96837390+Monspieeet@users.noreply.github.com>
2023-08-17 22:17:07 +0300

    Update data_list.rb

--- Core classes and models/data_list.rb
+++ Core classes and models/data_list.rb
@@ -1,24 +1,25 @@
 class Data_list
-    attr_reader :data
+  attr_reader :data, :selected
 
-    def initialize(data)
-      @data = data
-      @selected = []
-    end
-  
-    def select(number)
-      @selected << @data[number]
-    end
-  
-    def get_selected
-      @selected.map(&:id)
-    end
-  
-    def get_names
-      raise NotImplementedError
-    end
-  
-    def get_data
-      raise NotImplementedError
-    end
+
+  def initialize(data)
+    @data = data
+    @selected = []
+  end
+
+  def select(number)
+    @selected << data[:data][number]
+  end
+
+  def get_selected
+    @selected.map { |student| student[:id] }
+  end
+
+  def get_names
+    raise NotImplementedError
+  end
+
+  def get_data
+    raise NotImplementedError
+  end
 end 
