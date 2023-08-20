5319153aa4eeb7cfe2826b9c5386da90e118a6ff
Monspieeet <96837390+Monspieeet@users.noreply.github.com>
2023-08-20 23:19:00 +0300

    паттерн одиночка и бд

--- Core classes and models/student_list_DB.rb
+++ Core classes and models/student_list_DB.rb
@@ -1,34 +1,43 @@
 require_relative 'Student_list_base'
 require 'mysql2'
-require_relative 'singleton_DB'
+require_relative 'DB_singleton'
+require 'json'
 
 class Student_list_DB < Student_list_base
-  
-    private
-    def format_to_hash(str: )
-      return  JSON.parse(str)
-    end
-  
-    def hash_to_format(hash: )
-      values = hash.map { |h|
-        h.values.join ","
-      }.map { |str|
-        "(#{str})"
-      }.join(",")
-      return values
-    end
-  
-    def read_file(rfile:)
-      results = client.query("SELECT * FROM student", symbolize_keys: true)
-      return JSON.pretty_generate(results)
-    end
-  
-    def write_file(wfile:, data:)
-      insert_query = "insert into student values #{data}"
-      self.client.query insert_query
-    end
-  
-    attr_accessor client = DB_singleton.instance.db_client
-  
+  private
+  def list_hash_from_str(str: )
+    return  JSON.parse(str, {:symbolize_names => true})
   end
-end  
+
+  def list_hash_to_str(hash: )
+    values = hash.map { |h|
+      h.values.join ","
+    }.map { |str|
+      "(#{str})"
+    }.join(",")
+    return values
+  end
+
+  def read_file(rfile:)
+    results = client.query("SELECT * FROM student", symbolize_keys: true).to_a
+    puts results
+    return JSON.pretty_generate(results)
+  end
+
+  def write_to_file(wfile:, data:)
+    insert_query = "insert into student values #{data}"
+    self.client.query insert_query
+  end
+
+  public
+  attr_accessor :client
+  def initialize
+    self.client = Mysql2::Client.new(
+      host: 'localhost',
+      username: 'root',
+      password: '12345',
+      database: 'students'
+    )
+  end
+
+end
