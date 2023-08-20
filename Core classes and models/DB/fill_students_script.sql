978fc32aee3d7706c25b2676f362d047c773b688
Monspieeet <96837390+Monspieeet@users.noreply.github.com>
2023-08-20 21:45:23 +0300

    создание таблицы и заполнение

---
+++ Core classes and models/DB/fill_students_script.sql
@@ -0,0 +1,8 @@
+use students;
+truncate table student;
+
+insert into student(last_name, first_name, midle_name, phone_number, mail, telegram, git) values
+
+('Колайдин', 'Макар', 'Русланович', '+7-900-000-00-00', 'makar@gmail.com', '@makar', '@makar'),
+
+('Пушкина', 'Мария', 'Сергеевна', '+7-900-000-00-01', 'maria@mail.com', '@maria', '@maria')
