f1116a4fcf49ec55f295a6989c4bfcd3876e2ad0
Monspieeet <96837390+Monspieeet@users.noreply.github.com>
2023-08-20 23:19:48 +0300

    Update main.rb

--- Core classes and models/main.rb
+++ Core classes and models/main.rb
@@ -19,7 +19,7 @@ data1 = Data_list_student_short.new(data:obj_arr)
 
 @db_host  = "localhost"
 @db_user  = "root"
-@db_pass  = "1111"
+@db_pass  = "12345"
 @db_name = "students"
 
 client = Mysql2::Client.new(:host => @db_host, :username => @db_user, :password => @db_pass, :database => @db_name)
