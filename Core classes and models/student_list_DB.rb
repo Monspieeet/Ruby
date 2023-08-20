d2d122f03991276ef06d06dde7c5d1ecf33aba64
Monspieeet <96837390+Monspieeet@users.noreply.github.com>
2023-08-20 23:16:38 +0300

    паттерн singleton и бд

---
+++ Core classes and models/student_list_DB.rb
@@ -0,0 +1,34 @@
+require_relative 'Student_list_base'
+require 'mysql2'
+require_relative 'singleton_DB'
+
+class Student_list_DB < Student_list_base
+  
+    private
+    def format_to_hash(str: )
+      return  JSON.parse(str)
+    end
+  
+    def hash_to_format(hash: )
+      values = hash.map { |h|
+        h.values.join ","
+      }.map { |str|
+        "(#{str})"
+      }.join(",")
+      return values
+    end
+  
+    def read_file(rfile:)
+      results = client.query("SELECT * FROM student", symbolize_keys: true)
+      return JSON.pretty_generate(results)
+    end
+  
+    def write_file(wfile:, data:)
+      insert_query = "insert into student values #{data}"
+      self.client.query insert_query
+    end
+  
+    attr_accessor client = DB_singleton.instance.db_client
+  
+  end
+end  
