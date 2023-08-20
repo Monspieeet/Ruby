02e82a7383143291653db0a01478c0ac84f3401e
Monspieeet <96837390+Monspieeet@users.noreply.github.com>
2023-08-20 21:39:55 +0300

    Delete fill_student_script.sql

--- Core classes and models/DB/fill_student_script.sql
+++
@@ -1,8 +0,0 @@
-use students;
-truncate table student;
-
-insert into student(surname, name, father_name, phone, mail, tg, git) values
-
-('Колайдин', 'Макар', 'Русланович', '+7-900-000-00-00', 'makar@gmail.com', '@makar', '@makar'),
-
-('Пушкина', 'Мария', 'Сергеевна', '+7-900-000-00-01', 'maria@mail.com', '@maria', '@maria')
